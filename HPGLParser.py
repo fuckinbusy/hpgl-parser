fa792dfe2aadbe4c2ae503702779d11c928826f2
richard102 <40494935+fuckinbusy@users.noreply.github.com>
2023-08-23 14:14:41 +0300

    Add files via upload

--- HPGLParser.py
+++ HPGLParser.py
@@ -1,6 +1,7 @@
 from re import findall, DOTALL
 from math import hypot
 from tkinter import filedialog as fd
+import matplotlib.pyplot as plt
 
 path = fd.askopenfile(filetypes=[("HPGL", "*.plt")])
 
@@ -32,6 +33,35 @@ def getLength(filePath: str, pointsPerMm: int) -> int:
 
             total += length
 
-    return int(total/((pointsPerMm)*1000))
+    return int(total/(pointsPerMm*1000))
+
+def draw(filePath: str):
+    with open(filePath, 'rb') as file:
+        hpgl = file.read().decode('utf-8')
+
+        pattern = r"(LT;|SP\d+;)(.*?)(LT;|SP\d+;$)" 
+        objects = findall(pattern, hpgl, DOTALL)
+        
+        total = 0
+        for obj in objects:
+            c = obj[1]
+            x_ = []
+            y_ = []
+
+            code_pattern = r'(PU|PD)(-?\d+)\s(-?\d+)'
+            points = findall(code_pattern, c)
+
+            length = 0
+            prev_x, prev_y = map(int, points[0][1:])
+            for i in range(len(points)):
+                command, x, y = points[i]
+                x, y = map(int, (x, y))
+                x_.append(x)
+                y_.append(y)
+            plt.plot(x_, y_)
+
+
+    plt.show()
 
 print(getLength(path.name, 0.04))
+draw(path.name)
